c2790a570dea46fd5a4c92e9ed7d8cb947750f32
saba <sabasaeedakhtar@gmail.com>
2024-10-20 01:51:02 +1100

    created some service class methods

--- src/main/java/com/demo/vwap_calculator/dto/PriceData.java
+++ src/main/java/com/demo/vwap_calculator/dto/PriceData.java
@@ -5,11 +5,13 @@ import java.time.LocalTime;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
--- src/main/java/com/demo/vwap_calculator/service/VwapService.java
+++ src/main/java/com/demo/vwap_calculator/service/VwapService.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+
+import com.demo.vwap_calculator.dto.PriceData;
 import com.demo.vwap_calculator.dto.PriceDataResponse;
 import com.demo.vwap_calculator.entity.PriceDataEntity;
 import com.demo.vwap_calculator.repository.PriceDataRepository;
@@ -21,10 +23,29 @@ public class VwapService {
 	public PriceDataResponse getPriceData() {
 		log.info("Getting all Data from repository...!");
 		List<PriceDataEntity> priceDataEntityList= priceDataRepository.findAll();
+		List<PriceData> priceDataDto = this.maptoDTO(priceDataEntityList);
 		
-		//convert into DTO
-		return null;
+		return PriceDataResponse.builder().priceData(priceDataDto).build();
+	
 
+	}
+
+	private List<PriceData> maptoDTO(List<PriceDataEntity> priceDataEntityList) {
+		List<PriceData> priceDataDTO = new ArrayList<>();
+		priceDataEntityList.stream()
+		                   .forEach(entity-> 
+		                                    priceDataDTO.add(new PriceData( entity.getEntryNumber(),
+		                                            		                entity.getTimeStamp(), 
+		                                            		                entity.getCurrencyPair(),
+		                                            		                entity.getPrice(), 
+		                                            		                entity.getVolume()
+		                                            		               )
+				                                             )
+		                            );
+		
+		priceDataDTO.stream().forEach(System.out::println);
+		
+		return priceDataDTO;
 	}
 
 }
