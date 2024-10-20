8f5085aa684b149c319ad949c821a63f82db7d5b
saba <sabasaeedakhtar@gmail.com>
2024-10-20 21:47:25 +1100

    created some methods in service class and called it from controller class

--- src/main/java/com/demo/vwap_calculator/controller/VwapController.java
+++ src/main/java/com/demo/vwap_calculator/controller/VwapController.java
@@ -2,6 +2,7 @@ package com.demo.vwap_calculator.controller;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+import java.util.List;
 import java.util.concurrent.Callable;
 
 import org.springframework.http.HttpStatus;
@@ -11,28 +12,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.demo.vwap_calculator.dto.PriceData;
 import com.demo.vwap_calculator.dto.PriceDataResponse;
 import com.demo.vwap_calculator.service.VwapService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
-@RequestMapping(produces=APPLICATION_JSON_VALUE)
+@RequestMapping("/vwap")
 public class VwapController {
 	
 	private final VwapService vwapService;
 	
-	@GetMapping("/get-data")
-    public Callable<ResponseEntity<PriceDataResponse>> getCustomers() {
-
-        return new Callable<ResponseEntity<PriceDataResponse>>() {
-            @Override
-            public ResponseEntity<PriceDataResponse> call() throws Exception {
-            	PriceDataResponse response = vwapService.getPriceData();
-                return new ResponseEntity<>(response, HttpStatus.OK);
-            }
-        };
+	@GetMapping(produces= APPLICATION_JSON_VALUE, value = "v1/get-data")
+    public ResponseEntity<List> getExistingData() {     
+		
+		try {
+            	List<PriceData> dataResponse = vwapService.getPriceData();
+            	List<PriceDataResponse> priceDataResposne = vwapService.calculateHourlyVwap(dataResponse);
+                return new ResponseEntity<List>(priceDataResposne, HttpStatus.OK);
+		} catch (Exception ex) {
+			   log.error(ex.getMessage());
+	    	   throw ex;
+		   }  
+        
     }
+	
 
 }
--- src/main/java/com/demo/vwap_calculator/service/VwapService.java
+++ src/main/java/com/demo/vwap_calculator/service/VwapService.java
@@ -1,7 +1,10 @@
 package com.demo.vwap_calculator.service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -10,7 +13,6 @@ import com.demo.vwap_calculator.dto.PriceDataResponse;
 import com.demo.vwap_calculator.entity.PriceDataEntity;
 import com.demo.vwap_calculator.repository.PriceDataRepository;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,12 +23,14 @@ public class VwapService {
 	
 	private final PriceDataRepository priceDataRepository ;
 	
-	public PriceDataResponse getPriceData() {
+	public List<PriceData> getPriceData() {
+		
 		log.info("Getting all Data from repository...!");
 		List<PriceDataEntity> priceDataEntityList= priceDataRepository.findAll();
+		log.info("The PriceDTO is {}"+priceDataEntityList);
 		List<PriceData> priceDataDto = this.maptoDTO(priceDataEntityList);
 		
-		return PriceDataResponse.builder().priceData(priceDataDto).build();
+		return priceDataDto;
 	
 
 	}
@@ -43,10 +47,50 @@ public class VwapService {
 		                                            		               )
 				                                             )
 		                            );
-		
-		priceDataDTO.stream().forEach(System.out::println);
+	
 		
 		return priceDataDTO;
 	}
+
+	public List<PriceDataResponse> calculateHourlyVwap(List<PriceData> priceData) {
+		 List<PriceDataResponse> vwapList = new ArrayList<>();
+		 // Grouping by currency pair and hour
+        Map<String, Map<Integer, List<PriceData>>> groupedData = priceData.stream()
+            .collect(Collectors.groupingBy(
+                pd -> pd.getCurrencyPair(),
+                Collectors.groupingBy(PriceData::getHour)
+            ));
+    
+     // Calculate VWAP for each currency pair per hour
+        groupedData.forEach((currencyPair, hourlyData) -> {
+            hourlyData.forEach((hour, dataList) -> {
+                // Calculate the weighted price sum
+                double weightedPriceSum = dataList.stream()
+                    .mapToDouble(pd -> pd.getPrice() * pd.getVolume())
+                    .sum();
+                
+                // Calculate the total volume
+                int totalVolume = dataList.stream()
+                    .mapToInt(PriceData::getVolume)  // Access volume directly using method reference
+                    .sum();
+
+                // Calculate VWAP
+                double vwapCalculated = weightedPriceSum / totalVolume;
+               
+                 vwapList.add( PriceDataResponse.builder()
+                         .uniqueCurrencyPair(currencyPair)
+                         .hourlyData(hour)
+                         .vwap(vwapCalculated)
+                         .build());
+                
+            });          
+        });
+	
+		return vwapList;
+       
+	}
+
+	
+	
 
 }
